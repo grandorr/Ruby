3b46f32a3d2977dd942d79622920ebe11cecf71f
victorfouquet <victor.fouquet@gmail.com>
2019-07-09 14:48:11 +0200

    Modification syntaxe exo_05

--- exo_05.rb
+++ exo_05.rb
@@ -7,7 +7,7 @@ puts "Travail : #{10 * 5 * 11}"
 
 puts "En minutes ça fait : #{10 * 5 * 11 * 60}"
 # Permet d'afficher un message à l'écran, et la formule #{} permet d'intégrer un calcul
-# au sein de convertir le résultat en string et de l'intégrer au message.
+# et de convertir le résultat en string puis l'intégrer au message.
 
 puts "Et en secondes ?"
 # Permet d'afficher un message à l'écran.
@@ -24,10 +24,11 @@ puts 3 + 2 < 5 - 7
 
 puts "Ça fait combien 3 + 2 ? #{3 + 2}"
 # Permet d'afficher un message à l'écran, et la formule #{} permet d'intégrer un calcul
-# au sein de convertir le résultat en string et de l'intégrer au message.
+# et de convertir le résultat en string puis l'intégrer au message.
+
 puts "Ça fait combien 5 - 7 ? #{5 - 7}"
 # Permet d'afficher un message à l'écran, et la formule #{} permet d'intégrer un calcul
-# au sein de convertir le résultat en string et de l'intégrer au message.
+# et de convertir le résultat en string puis l'intégrer au message.
 
 puts "Ok, c'est faux alors !"
 # Permet d'afficher un message à l'écran.
@@ -37,18 +38,18 @@ puts "C'est drôle ça, faisons-en plus :"
 
 puts "Est-ce que 5 est plus grand que -2 ? #{5 > -2}"
 # Permet d'afficher un message à l'écran, et la formule #{} permet d'intégrer un calcul
-# au sein de convertir le résultat en string et de l'intégrer au message.
+# et de convertir le résultat en string puis l'intégrer au message.
 #Le symbôle ">" permet de vérifier la véracité de l'opération.
 #Si l'opération est juste, le terminal confirme par "true", sinon il infirme par "false".
 
 puts "Est-ce que 5 est supérieur ou égal à -2 ? #{5 >= -2}"
 # Permet d'afficher un message à l'écran, et la formule #{} permet d'intégrer un calcul
-# au sein de convertir le résultat en string et de l'intégrer au message.
+# et de convertir le résultat en string puis l'intégrer au message.
 #Le symbôle ">=" permet de vérifier la véracité de l'opération.
 #Si l'opération est juste, le terminal confirme par "true", sinon il infirme par "false".
 
 puts "Est-ce que 5 est inférieur ou égal à -2 ? #{5 <= -2}"
 # Permet d'afficher un message à l'écran, et la formule #{} permet d'intégrer un calcul
-# au sein de convertir le résultat en string et de l'intégrer au message.
+# et de convertir le résultat en string puis l'intégrer au message.
 #Le symbôle "<=" permet de vérifier la véracité de l'opération.
 #Si l'opération est juste, le terminal confirme par "true", sinon il infirme par "false".
